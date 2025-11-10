073f1d6947eff4a36959769bb4c92490d91bdf4f
samwongpq <154324549+samwongpq@users.noreply.github.com>
2025-11-10 19:13:38 +0800

    Update print.py

--- print.py
+++ print.py
@@ -6,6 +6,6 @@ import os
 # Get the API key from the environment variable
 api_key = os.environ.get('API_KEY')
 api_secret = os.environ.get('API_SECRET')
-
+print(api_key)
 if not api_key or not api_secret:
     raise ValueError("API_KEY and API_SECRET environment variables not set!")
