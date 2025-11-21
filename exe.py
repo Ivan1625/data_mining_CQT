5e40fe48c6a8da33bb93a6370a774d8574394cbd
Ivan1625 <160002396+Ivan1625@users.noreply.github.com>
2025-11-22 00:16:49 +0800

    Update exe.py

--- exe.py
+++ exe.py
@@ -83,7 +83,7 @@ class Execution:
             side='SELL'
         payload = {
             "timestamp": int(time.time()) * 1000,
-            "pair": coin + "/USD",
+            "pair": "SOL" + "/USD",
             "side": side,
             "quantity": qty,
         }
